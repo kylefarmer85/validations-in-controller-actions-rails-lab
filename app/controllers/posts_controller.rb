2d605c1f563fb3f6c582bdee3acd9d1213d118a6
kylefarmer85 <kylefarmer85@gmail.com>
2020-10-14 12:16:05 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,23 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
